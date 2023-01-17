6890ac8e9ada21bd5c1fec9e277d83fbef6ecddc
Ebun Ahme <EBunAhme@Ebuns-MacBook-Pro.local>
2023-01-17 23:58:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,200 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+  
+  
+  // Function to prompt user for password options
+  function getPasswordOptions() {
+    let length = parseInt(
+      prompt ("How many characters would you like your password to have?")
+    )
+  
+    if(isNaN(length) === true){
+      alert('Must be provided as number');
+      return;
+    }
+    if(length < 10) {
+      alert('Must be at least 10 characters');
+      return;
+    }
+    if(length > 64) {
+      alert ('Must be less than 65 characters');
+      return;
+    }
+    let hasSpecialCharacters = confirm(
+      "Click OK if you'd like to include special characters"
+    )
+    let hasNumericCharacters = confirm(
+      "Click OK if you'd like to include numeric characters"
+    )
+    let hasLowerCasedCharacters = confirm(
+      "Click OK if you'd like to include lowercase characters"
+    )
+    let hasUpperCasedCharacters = confirm(
+      "Click OK if you'd like to include uppercase characters"
+    )
+      if(hasUpperCasedCharacters === false &&
+        hasLowerCasedCharacters === false &&
+        hasNumericCharacters === false &&
+        hasSpecialCharacters === false ){
+          alert('Please chose at least one character type');
+          return;
+        }
+  
+        let getPasswordOptions = {
+          length: length,
+          hasSpecialCharacters: hasSpecialCharacters,
+          hasUpperCasedCharacters: hasUpperCasedCharacters,
+          hasLowerCasedCharacters: hasLowerCasedCharacters,
+          hasNumericCharacters: hasNumericCharacters,
+        }
+  
+        return getPasswordOptions;
+  }
+  
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+      let randomIndex = Math.floor(Math.random() * arr.length)
+      let randomElement = arr[randomIndex];
+      return randomElement; 
+      }
+  
+  
+  // Function to generate password with user input
+  function generatePassword() {
+      let options = getPasswordOptions();
+      console.log(options);
+      let result = []
+      let possibleCharacter = []
+      let guaranteedCharacter = []
+    
+      if(options.hasSpecialCharacters) {
+        possibleCharacter = possibleCharacter.concat(specialCharacters);
+        guaranteedCharacter.push(getRandom(specialCharacters))
+      }
+    
+      if(options["hasLowerCasedCharacters"]) {
+        possibleCharacter = possibleCharacter.concat(lowerCasedCharacters);
+        guaranteedCharacter.push(getRandom(lowerCasedCharacters))
+      }
+    
+      if(options.hasUpperCasedCharacters) {
+        possibleCharacter = possibleCharacter.concat(upperCasedCharacters);
+        guaranteedCharacter.push(getRandom(upperCasedCharacters))
+      }
+    
+      if(options.hasNumericCharacters) {
+        possibleCharacter = possibleCharacter.concat(numericCharacters);
+        guaranteedCharacter.push(getRandom(numericCharacters))
+      }
+    
+      for(let index = 0; index < options.length; index ++){
+        var generated = getRandom(possibleCharacter);
+        console.log(generated);
+        result.push(generated);
+      }
+      console.log(result);
+    
+    return result.join("")
+    }
+    
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
